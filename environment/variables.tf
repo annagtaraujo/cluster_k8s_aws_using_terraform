fc1e182f57a124c645deb4fb555c5f986c052828
Anna Gabriela Araujo <annagtaraujo@gmail.com>
2022-09-05 18:50:35 -0300

    Update variables.tf

--- environment/variables.tf
+++ environment/variables.tf
@@ -32,12 +32,6 @@ variable "instance_type"{
   default = "t2.medium"
 }
 
-variable "public_key"{
-  type = string
-  description = "Public key de acesso às instâncias públicas"
-  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCpuAcCDGl/ONwyusp4q34RVPA5py/1Acj6zPHQcMnPJsbAajg5qpEunxncmPio35Qf1Q5HcVxpK2IpSRVJUH3C8n8EUWPvOZIBApfTcGumUO1UhTQr7lv/3Jthpn3jYivLYk8RsRj55noW/HbWVesEnitWQmAIhi169t/yAknPH6cyXImJe6o6WfbMU/6h8iZ7XLYbt8AmcXRK0oUgY9m9TFvQHVW2SOdhNzI+LI+YeBovNV+esDDMb7s7fpo+dFolubslyhbOlt09W7iLIDvUh+/1ooYtt5NOGDlQlbgEQqpVnd5xuL/8mU144VGvrJT1NbRLKf4VRHa5jfe4GTh7 annagtaraujo@BRRIOWN021449"
-}
-
 variable "key_name"{
   type = string
   description = "Nome da KeyPair"
