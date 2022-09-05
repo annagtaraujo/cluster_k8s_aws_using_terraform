acda2c5c639ed55b45a89d3ed9bd96ff0c3ecf2d
Anna Gabriela Araujo <annagtaraujo@gmail.com>
2022-09-05 18:50:15 -0300

    Update key.tf

--- environment/key.tf
+++ environment/key.tf
@@ -2,7 +2,6 @@ module "ingress-keypair" {
   source = "../modules/key"
 
   key_name   = var.key_name
-  public_key = var.public_key
 
   tags = {
       Name = format("%s-publick-key", var.infra_name)
